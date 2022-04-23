51e6f1767ef217621fef3d12b18fc375914b4d07
Glitch (sixth-chocolate-honeydew) <none>
2022-04-23 03:47:13 +0000

    🌌🐁 Updated with Glitch

--- script.js
+++ script.js
@@ -4,35 +4,43 @@ const nextClueWaitTime = 1000; //how long to wait before starting playback of th
 
 //Global Variables
 var pattern = [];
-var progress = 0; 
+var progress = 0;
 var gamePlaying = false;
 var tonePlaying = false;
 var guessCounter = 0;
 var clueHoldTime = 1000; //how long to hold each clue's light/sound
-var volume = 0.7;  //must be between 0.0 and 1.0
+var volume = 0.7; //must be between 0.0 and 1.0
 var mistakes = 0;
+var moves = 0;
+var mist = 0;
 
-function startGame(){
-    //initialize game variables
-    progress = 0;
-    gamePlaying = true;
-    mistakes = 0;
+function startGame() {
+  //initialize game variables
+  progress = 0;
+  gamePlaying = true;
+  mistakes = 0;
+  pattern = [];
+  moves = 0;
+  mist = 0;
+
   
   for (let i = 0; i < 6; i++) {
     pattern.push(randomSeq(5));
   }
   // swap the Start and Stop buttons
-    document.getElementById("startBtn").classList.add("hidden");
-    document.getElementById("stopBtn").classList.remove("hidden");
-    playClueSequence()
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+  playClueSequence();
 }
 
-function stopGame(){
-    //initialize game variables
-    gamePlaying = false;
+function stopGame() {
+  //initialize game variables
+  
+  gamePlaying = false;
   // swap the Start and Stop buttons
-    document.getElementById("startBtn").classList.remove("hidden");
-    document.getElementById("stopBtn").classList.add("hidden");
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+  window.location.reload();
 }
 
 // Sound Synthesis Functions
@@ -41,100 +49,104 @@ const freqMap = {
   2: 329.6,
   3: 392,
   4: 466.2,
-  5: 520.0
+  5: 520.0,
+};
+function playTone(btn, len) {
+  o.frequency.value = freqMap[btn];
+  g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+  context.resume();
+  tonePlaying = true;
+  setTimeout(function () {
+    stopTone();
+  }, len);
 }
-function playTone(btn,len){ 
-  o.frequency.value = freqMap[btn]
-  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
-  context.resume()
-  tonePlaying = true
-  setTimeout(function(){
-    stopTone()
-  },len)
-}
-function startTone(btn){
-  if(!tonePlaying){
-    context.resume()
-    o.frequency.value = freqMap[btn]
-    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
-    context.resume()
-    tonePlaying = true
+function startTone(btn) {
+  if (!tonePlaying) {
+    context.resume();
+    o.frequency.value = freqMap[btn];
+    g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+    context.resume();
+    tonePlaying = true;
   }
 }
-function stopTone(){
-  g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
-  tonePlaying = false
+function stopTone() {
+  g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025);
+  tonePlaying = false;
 }
 
 // Page Initialization
 // Init Sound Synthesizer
-var AudioContext = window.AudioContext || window.webkitAudioContext 
-var context = new AudioContext()
-var o = context.createOscillator()
-var g = context.createGain()
-g.connect(context.destination)
-g.gain.setValueAtTime(0,context.currentTime)
-o.connect(g)
-o.start(0)
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var o = context.createOscillator();
+var g = context.createGain();
+g.connect(context.destination);
+g.gain.setValueAtTime(0, context.currentTime);
+o.connect(g);
+o.start(0);
 
-function lightButton(btn){
-  document.getElementById("button"+btn).classList.add("lit")
+function lightButton(btn) {
+  document.getElementById("button" + btn).classList.add("lit");
 }
-function clearButton(btn){
-  document.getElementById("button"+btn).classList.remove("lit")
+function clearButton(btn) {
+  document.getElementById("button" + btn).classList.remove("lit");
 }
 
-function playSingleClue(btn){
-  if(gamePlaying){
+function playSingleClue(btn) {
+  if (gamePlaying) {
     lightButton(btn);
-    playSound(btn,clueHoldTime);
-    setTimeout(clearButton,clueHoldTime,btn);
+    playSound(btn, clueHoldTime);
+    setTimeout(clearButton, clueHoldTime, btn);
   }
 }
 
-function playClueSequence(){
-  context.resume()
+function playClueSequence() {
+  context.resume();
   let delay = nextClueWaitTime; //set delay to initial wait time
   guessCounter = 0;
-  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
-    console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
-    setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
+  for (let i = 0; i <= progress; i++) {
+    // for each clue that is revealed so far
+    console.log("play single clue: " + pattern[i] + " in " + delay + "ms");
+    setTimeout(playSingleClue, delay, pattern[i]); // set a timeout to play that clue
     clueHoldTime -= 10;
-    delay += clueHoldTime 
+    delay += clueHoldTime;
     delay += cluePauseTime;
   }
 }
 
-function loseGame(){
+function loseGame() {
   stopGame();
   alert("Game Over. You lost.");
+
 }
-function winGame(){
+function winGame() {
   stopGame();
   alert("Game Over. the Dub, Yessir!.");
+
 }
 
-function guess(btn){
+function guess(btn) {
   console.log("user guessed: " + btn);
-  if(!gamePlaying){
+  if (!gamePlaying) {
     return;
   }
   var leng = pattern.length - 1;
   var checker = pattern[guessCounter];
-  if(checker == btn){
-    if(guessCounter == progress){
-      if(progress == leng){
+  if (checker == btn) {
+    if (guessCounter == progress) {
+      if (progress == leng) {
         winGame();
-      }else{
+      } else {
         progress += 1;
         playClueSequence();
       }
-    }else{
+    } else {
       guessCounter += 1;
     }
   } else if (checker != btn && mistakes < 2) {
     mistakes += 1;
-  }else{
+    addMist();
+  } else {
     loseGame();
   }
 }
@@ -146,14 +158,14 @@ function randomSeq(arr) {
 function playSound(btn, len) {
   switch (btn) {
     case 1:
-      document.getElementById("levi").play();
+      document.getElementById("eren").play();
       setTimeout(function () {
         stopTone();
       }, len);
       break;
 
     case 2:
-      document.getElementById("armin").play();
+      document.getElementById("levi").play();
       tonePlaying = true;
       setTimeout(function () {
         stopTone();
@@ -161,7 +173,7 @@ function playSound(btn, len) {
       break;
 
     case 3:
-      document.getElementById("hange").play();
+      document.getElementById("mikasa").play();
       tonePlaying = true;
       setTimeout(function () {
         stopTone();
@@ -169,7 +181,7 @@ function playSound(btn, len) {
       break;
 
     case 4:
-      document.getElementById("eren").play();
+      document.getElementById("armin").play();
       tonePlaying = true;
       setTimeout(function () {
         stopTone();
@@ -177,7 +189,7 @@ function playSound(btn, len) {
       break;
 
     case 5:
-      document.getElementById("mikasa").play();
+      document.getElementById("hange").play();
       tonePlaying = true;
       setTimeout(function () {
         stopTone();
@@ -185,27 +197,47 @@ function playSound(btn, len) {
       break;
   }
 }
+// adds a moves made counter
+function addCount() {
+  moves += 1;
+  document.getElementById("clickme").innerHTML = "Moves: " + moves;
+}
+
+var button1 = document.getElementById("mist"),
+  mist = 0;
+function addMist() {
+  mist += 1;
+  button1.innerHTML = "Mistakes: " + mist;
+}
+
+
+
 
 function startSound(btn) {
   switch (btn) {
     case 1:
-      document.getElementById("levi").play();
+      document.getElementById("eren").play();
+      addCount();
       break;
 
     case 2:
-      document.getElementById("armin").play();
+      document.getElementById("levi").play();
+      addCount();
       break;
 
     case 3:
-      document.getElementById("hange").play();
+      document.getElementById("mikasa").play();
+      addCount();
       break;
 
     case 4:
-      document.getElementById("eren").play();
+      document.getElementById("armin").play();
+      addCount();
       break;
 
     case 5:
-      document.getElementById("mikasa").play();
+      document.getElementById("hange").play();
+      addCount();
       break;
   }
 }
